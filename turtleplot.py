19e8126ca163165fc773033d8a0eec920a3efa4e
Nikolaj Søndergaard Povlsen <s183939@student.dtu.dk>
2018-12-06 10:37:07 +0100

    turtlePlot viser 'noget' plot

--- turtleplot.py
+++ turtleplot.py
@@ -6,6 +6,7 @@ Created on Wed Dec  5 21:30:48 2018
 @author: nik
 """
 import numpy as np
+import matplotlib.pyplot as plt
 
 def turtlePlot(turtleCommands):
     
@@ -22,12 +23,12 @@ def turtlePlot(turtleCommands):
     # and creates a single array for accessing angles
     angles = np.array([turtleCommands[i] for i in range(len(turtleCommands)) if i % 2 == 1])
     
-    #length = 1/N
+    length = np.array([turtleCommands[i]/2 for i in range(len(turtleCommands)) if i % 2 == 0])
     
 
-    
-    for i in range(0,1):
-        print(i)
+    # Calculate next direction vector 'Orientation'
+    for i in range(0,len(angles)):
+
         # Defining cos and sin functions for direction handling
         cos = np.cos(angles[i])
         sin = np.sin(angles[i])
@@ -37,14 +38,30 @@ def turtlePlot(turtleCommands):
 
 
         # Calculating the matrix.vector product for new orientation
-        dot = np.dot(d, Orientation[:,i])
+        dot = np.array(np.dot(d, Orientation[:,i]),copy=False, subok=True, ndmin=2).T
         
         # Appending new orientation to array of orientaions
-        Orientation = np.append((Orientation,dot),1)
-                    
-                
-
-    return Orientation
+        Orientation = np.concatenate(( Orientation, dot), axis=1)
         
     
+    # Calculate next vector to be plottet
+    for i in range(1,len(length)):
+        
+        #Calculate the vector
+        vector = np.array(xy[:,i-1]+length[i]*Orientation[:,i], copy=False, subok=True, ndmin=2).T
+
+        # Appending the new vector to vector 'xy' array
+        xy = np.concatenate((xy, vector), axis=1)
+        
+        
+    # Plotting with matplotlib
+    plt.plot(xy[1].T)
+    plt.ylabel('Korch')
+    plt.show()
+
+    return
+        
+
+# Below line is for testing purpose
+# [print('Coordinates for {}-vector, is {}'.format(i, korch2[:,i])) for i in range(0,16)]    
     
